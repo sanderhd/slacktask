b7491b374d24f1724334cc73087db29351db57af
sanderhd <sanderhoudijk10@gmail.com>
2026-05-01 11:40:27 +0200

    feat: create nice message for new task with button

--- commands/task.js
+++ commands/task.js
@@ -4,6 +4,37 @@ module.exports = (app) => {
 
         const task = command.text;
 
-        await respond(`Created Task: *${task}*`)
+        await respond({
+            blocks: [
+                {
+                    type: "header",
+                    text: {
+                        type: "plain_text",
+                        text: "📝 New Task Created",
+                    },
+                },
+                {
+                    type: "section",
+                    text: {
+                        type: "mrdown",
+                        text: command.text,
+                    },
+                },
+                {
+                    type: "actions",
+                    elements: [
+                        {
+                            type: "button",
+                            text: {
+                                type: "plain_text",
+                                text: "✅ Done"
+                            },
+                            style: "primary",
+                            action_id: "task_done",
+                        },
+                    ],
+                },
+            ],
+        });
     });
 };
